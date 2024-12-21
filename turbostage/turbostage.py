d893ff68edabd44c24c8b8e68d537c97c89b74fa
jberclaz <jerome@percipient.ai>
2024-12-20 20:54:58 -0800

    created ui

---
+++ turbostage/turbostage.py
@@ -0,0 +1,38 @@
+import sys
+
+from main_window import MainWindow
+from PySide6.QtCore import Qt, QTimer
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QSplashScreen
+
+
+def show_splash_screen():
+    # Load an image for the splash screen
+    pixmap = QPixmap("../content/turbostage.png")  # Replace with the path to your image
+
+    # Create the splash screen with the image
+    splash = QSplashScreen(pixmap, Qt.WindowStaysOnTopHint)
+    splash.setMask(pixmap.mask())
+
+    # Add text on top of the image
+    splash.showMessage("Loading...", alignment=Qt.AlignBottom | Qt.AlignCenter, color=Qt.white)
+
+    # Show the splash screen
+    splash.show()
+
+    return splash
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    splash = show_splash_screen()
+    window = MainWindow()
+
+    QTimer.singleShot(2000, lambda: [splash.close(), window.show()])
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
